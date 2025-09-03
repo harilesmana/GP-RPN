fcf86920db372d4ce810ebf39a772d67855120ff
harilesmana <harilesmana878@gmail.com>
2025-09-03 23:06:31 +0700

    Update jwt.ts

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken';
 
-const JWT_SECRET = process.env.JWT_SECRET || 'your-secret-key';
+const JWT_SECRET = process.env.JWT_SECRET || 'abogoboga';
 const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || '1d';
 
 export interface JwtPayload {
