c0c824b19c3586eab0a8c784aee2c62a44238e8f
harilesmana <harilesmana878@gmail.com>
2025-09-03 22:52:09 +0700

    Update hash.ts

--- src/utils/hash.ts
+++ src/utils/hash.ts
@@ -1,23 +1,10 @@
-import bcrypt from "bcryptjs";
+import bcrypt from 'bcryptjs';
 
-const SALT_ROUNDS = 12;
+export const hashPassword = async (password: string): Promise<string> => {
+  const saltRounds = 10;
+  return await bcrypt.hash(password, saltRounds);
+};
 
-export async function hashPassword(password: string): Promise<string> {
-  if (password.length < 6) {
-    throw new Error("Password harus minimal 6 karakter");
-  }
-  return await bcrypt.hash(password, SALT_ROUNDS);
-}
-
-export async function verifyPassword(password: string, hash: string): Promise<boolean> {
+export const comparePassword = async (password: string, hash: string): Promise<boolean> => {
   return await bcrypt.compare(password, hash);
-}
-
-export function validatePasswordStrength(password: string): boolean {
-  const minLength = 6;
-  const hasUpperCase = /[A-Z]/.test(password);
-  const hasLowerCase = /[a-z]/.test(password);
-  const hasNumbers = /\d/.test(password);
-  
-  return password.length >= minLength && (hasUpperCase || hasLowerCase) && hasNumbers;
-}
+};
