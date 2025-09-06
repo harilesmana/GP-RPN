70b9823e944c42969c123ee5c4dd0b37c024e389
harilesmana <harilesmana878@gmail.com>
2025-09-07 02:16:24 +0700

    update src/utils/hash.ts

--- src/utils/hash.ts
+++ src/utils/hash.ts
@@ -1,18 +1,14 @@
 import bcrypt from "bcryptjs";
 
-
 export async function hashPassword(plain: string, saltRounds = 10) {
   const salt = await bcrypt.genSalt(saltRounds);
   return bcrypt.hash(plain, salt);
 }
 
-
 export async function verifyPassword(plain: string, hash: string) {
   return bcrypt.compare(plain, hash);
 }
 
-
 export function validatePasswordStrength(pw: string) {
-  
   return pw.length >= 6;
 }
