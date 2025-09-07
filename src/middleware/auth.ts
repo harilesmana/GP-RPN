d6ea21fe29eb9548b31c44a1994ef3bec1fa295d
harilesmana <harilesmana878@gmail.com>
2025-09-07 17:54:29 +0700

    update src/middleware/auth.ts

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,7 +1,7 @@
-import { Elysia, type Context } from "elysia";
+import type { Context } from "elysia";
 import { verifySession } from "../utils/session";
 
-export const authMiddleware = new Elysia().derive(({ cookie, set, request }) => {
+export async function authMiddleware({ cookie, set, request }: Context) {
   const token = cookie?.session?.value;
   if (!token) {
     return { user: null };
@@ -13,11 +13,12 @@ export const authMiddleware = new Elysia().derive(({ cookie, set, request }) => 
     if (cookie?.session) cookie.session.set({ value: "", maxAge: 0 });
     return { user: null };
   }
-
-  const user = {
-    userId: data.userId,
-    role: data.role,
-  }
-
-  return { user };
-})
+  
+  return { 
+    user: {
+      userId: data.userId,
+      role: data.role,
+      
+    } 
+  };
+}
