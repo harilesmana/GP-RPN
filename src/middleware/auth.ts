b54e92908fc4221b49b954b967a569a386c3aecd
harilesmana <harilesmana878@gmail.com>
2025-09-08 21:18:03 +0700

    update src/middleware/auth.ts

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,5 +1,6 @@
 import type { Context } from "elysia";
 import { verifySession } from "../utils/session";
+import { users } from "../db";
 
 export async function authMiddleware({ cookie, set, request }: Context) {
   const token = cookie?.session?.value;
@@ -14,11 +15,22 @@ export async function authMiddleware({ cookie, set, request }: Context) {
     return { user: null };
   }
   
+  
+  const userData = users.find(u => u.id === data.userId);
+  
+  if (!userData) {
+    if (cookie?.session) cookie.session.set({ value: "", maxAge: 0 });
+    return { user: null };
+  }
+  
   return { 
     user: {
       userId: data.userId,
+      nama: userData.nama,
+      email: userData.email,
       role: data.role,
-      
+      bidang: userData.bidang,
+      kelas_id: userData.kelas_id
     } 
   };
 }
